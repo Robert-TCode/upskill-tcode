c5794defbf7db95238700d21e81161c839b1ceb9
Robert Tanase <robert@crowdconnected.com>
2020-12-28 17:06:36 +0000

    Add Travis yml file

--- UpSkillTests/UpSkillTests.swift
+++ UpSkillTests/UpSkillTests.swift
@@ -19,15 +19,7 @@ class UpSkillTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual(1+1, 2)
     }
 
 }
